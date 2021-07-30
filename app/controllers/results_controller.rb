022a5d01e7cf6ccdcbbac9b3fb4c1ae33ddfdb8f
ok-akiyama <ok.akiyama.tom@gmail.com>
2021-07-30 13:59:11 +0900

    index.html記述

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,5 +1,11 @@
 class ResultsController < ApplicationController
+
   def index
     @result = Result.all
   end
+
+  def new
+    @tweet = Tweet.new
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  root to: 'results#index'
   resources :results
 end
