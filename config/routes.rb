9a6646d5af7614209e3a68eace00200199115764
ok-akiyama <ok.akiyama.tom@gmail.com>
2021-08-07 21:13:51 +0900

    ヘッダールート設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'results#index'
   resources :results
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
