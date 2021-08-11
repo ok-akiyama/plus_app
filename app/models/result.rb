954969a4ae78bf0ebcdb2daf9cae5f507b1447dc
ok-akiyama <ok.akiyama.tom@gmail.com>
2021-08-11 12:12:42 +0900

    view.application.html.erbインデント

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,7 +1,7 @@
 class Result < ApplicationRecord
   has_one_attached :image
 
-  validates :name, presence: true
-  validates :text, presence: true
+  validates :name,  presence: true
+  validates :text,  presence: true
   validates :image, presence: true
 end
