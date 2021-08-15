1aff19b0f9b8aa26d8ded82f3c16969a76fcc352
ok-akiyama <ok.akiyama.tom@gmail.com>
2021-08-15 22:52:24 +0900

    画像情報の取得

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){
   const ImageList = document.getElementById('image-list');
+  document.getElementById('item-image').addEventListener('change', function(e){
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+  });
 });
