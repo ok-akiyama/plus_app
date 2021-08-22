3324de6588b4564ccfadade8d79c805fff4987fc
ok-akiyama <ok.akiyama.tom@gmail.com>
2021-08-22 19:39:15 +0900

    バリデーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :results
 
-  validates :nickname, presence: true
+  validates :nickname, presence: true, length: { maximum: 6 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,18 @@ RSpec.describe User, type: :model do
   end
 
   describe 'ユーザー新規登録' do
+    it 'nicknameとemail、passwordとpassword_confirmationが存在すれば登録できる' do
+      expect(@user).to be_valid
+    end
+    it 'nicknameが6文字以下であれば登録できる' do
+      @user.nickname = 'aaaaaa'
+      expect(@user).to be_valid
+    end
+    it 'passwordとpassword_confirmationが6文字以上であれば登録できる' do
+      @user.password = '000000'
+      @user.password_confirmation = '000000'
+      expect(@user).to be_valid
+    end
     it 'nicknameが空では登録できない' do
       @user.nickname = ''
       @user.valid?
@@ -15,6 +27,16 @@ RSpec.describe User, type: :model do
       @user.email = ''
       @user.valid?
       expect(@user.errors.full_messages).to include("メールアドレスが入力されていません。")
+    end
+    it 'passwordが空では登録できない' do
+    end
+    it 'passwordとpassword_confirmationが不一致では登録できない' do
+    end
+    it 'nicknameが7文字以上では登録できない' do
+    end
+    it '重複したemailが存在する場合登録できない' do
+    end
+    it 'passwordが5文字以下では登録できない' do
     end
   end
 end
